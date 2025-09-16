ea3e5c09bfe73043efa285fd2bc68f061e3e82b5
itzik templeman <itzik.templeman@gmail.com>
2025-09-16 12:07:50 +0300

    f

--- Frontend/src/Components/LayoutArea/Routing/Routing.tsx
+++ Frontend/src/Components/LayoutArea/Routing/Routing.tsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { Page404 } from "../../Screens/Page404/Page404";
+
 import { MyProjects } from "../../Screens/MyProjects/MyProjects";
 import Profile from "../../Screens/Profile/Profile";
 
@@ -9,8 +9,7 @@ export function Routing() {
         <div>
             <Routes>
                 <Route path="/" element={<Navigate to="/my-projects" />} />
-                <Route path="*" element={<Page404 />} />
-            
+                <Route path="*" element={<Navigate to="/my-projects" />} />
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/my-projects" element={<MyProjects />} />
             </Routes>
