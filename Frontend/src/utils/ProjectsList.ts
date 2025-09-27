b4ebaa156c3079a331e7796ab79c49dd47d4d6f3
itzik templeman <itzik.templeman@gmail.com>
2025-09-27 12:22:39 +0300

    d

--- Frontend/src/utils/ProjectsList.ts
+++ Frontend/src/utils/ProjectsList.ts
@@ -1,40 +1,24 @@
 import { Project } from "../models/Project";
+import { asset } from "../utils/asset";
 
 class ProjectList {
-
-    public projects: Project[] = [
-        new Project(
-            1,
-            "Vacations",
-            "Vacations management website with a fullstack system with front " +
-            "and backend\n - admins can manage ta vacation fully" +
-            "(add, edit, remove and view like charts per destination by users)\n" +
-            "while regular users can view and like or unlike vacations)",
-            "assets/images/vacations-site-image.png",
-            "https://github.com/ItzikTempleman/vacations"
-        ),
-        new Project(
-            2,
-            "Weather and forecast",
-            "An intuitive weather website that shows city temperatures in israel locally",
-            "assets/images/weather-site-image.png",
-            "https://github.com/ItzikTempleman/WeatherForecast"
-        ),
-        new Project(
-            3,
-            "Krypto-King",
-            "A crypto currency website to compare prices of currencies intuitively with a chart",
-            "assets/images/crypto.png",
-            "https://github.com/ItzikTempleman/Crypto"
-        ),
-        new Project(
-            4,
-            "Notes",
-            "A nicely organized note tracking app with functionalities of saving, editing and deleting and font changing. Enjoy",
-            "assets/images/notes.png",
-            "https://github.com/ItzikTempleman/note-tracker"
-        ),
-    ];
+  public projects: Project[] = [
+    new Project(
+      1,
+      "Vacations",
+      "Vacations management website…",
+      asset("assets/images/vacations-site-image.png"),
+      "https://github.com/ItzikTempleman/vacations"
+    ),
+    new Project(
+      2,
+      "Weather and forecast",
+      "An intuitive weather website…",
+      asset("assets/images/weather-site-image.png"),
+      "https://github.com/ItzikTempleman/WeatherForecast"
+    ),
+    // …
+  ];
 }
 
 export const projectList = new ProjectList();
