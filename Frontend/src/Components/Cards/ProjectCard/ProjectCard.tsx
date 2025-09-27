d75a6df75525d96a80971cdfc7a69104bee5f5ac
itzik templeman <itzik.templeman@gmail.com>
2025-09-27 12:34:14 +0300

    d

--- Frontend/src/Components/Cards/ProjectCard/ProjectCard.tsx
+++ Frontend/src/Components/Cards/ProjectCard/ProjectCard.tsx
@@ -4,10 +4,9 @@ import "./ProjectCard.css";
 type ProjectProps = { project: Project };
 
 export function ProjectCard({ project }: ProjectProps) {
-  const src = `${import.meta.env.BASE_URL}${project.image?.replace(/^\/+/, "")}`;
   return (
     <div className="ProjectCard">
-      <img className="project-cover" src={src} alt={project.title} />
+      <img className="project-cover" src={project.image!} alt={project.title} />
       <a href={project.url}>Github link to project</a>
       <h3>{project.title}</h3>
       <p>{project.description}</p>
